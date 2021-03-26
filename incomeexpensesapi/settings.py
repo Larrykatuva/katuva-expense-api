342e715908322f99d4b9f8b1f60faabc8002c251
Larrykatuva <>
2021-03-26 11:48:06 +0300

    Satatic files

--- incomeexpensesapi/settings.py
+++ incomeexpensesapi/settings.py
@@ -151,10 +151,17 @@ USE_L10N = True
 USE_TZ = True
 
 
+BASE_DR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 # Static files (CSS, JavaScript, Images)
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'incomeexpensesapi/static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DR, 'static'),
+)
 
 
 # Email setup
